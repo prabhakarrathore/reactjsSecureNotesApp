a23084638cc3f626ddbfdfb2e5819660fe7b1a3c
prabhakar <prabhakarrathore15@gmail.com>
2025-02-23 12:29:42 +0530

    backend change

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-const BASE_URL = 'https://backendfornotesapp-1.onrender.com';
+// const BASE_URL = 'https://backendfornotesapp-1.onrender.com';
+const BASE_URL = 'https://backend-for-notes-app.vercel.app';
 
 export default axios.create({
     baseURL: BASE_URL
